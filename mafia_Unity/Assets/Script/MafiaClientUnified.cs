e0fa15f608ecef03c5f7a7375b5a7f5cba2c8591
koossin <koossin1229@gmail.com>
2025-06-16 09:52:44 +0900

    거의다함

--- mafia_Unity/Assets/Script/MafiaClientUnified.cs
+++ mafia_Unity/Assets/Script/MafiaClientUnified.cs
@@ -287,6 +287,7 @@ public class MafiaClientUnified : MonoBehaviour
         }
 
         websocket = new WebSocket("ws://localhost:3000");
+        //websocket = new WebSocket("ws://3.37.55.237:3000");
 
         websocket.OnOpen += () =>
         {
